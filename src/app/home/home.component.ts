e55bd1764642d99f871ba3c3131f788b2ddd934c
AndreyDov <dovban.andreyy@rambler.ru>
2024-06-12 19:24:46 +0300

    fix: подшотовка к добавлению transloco

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,53 +1,23 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
-import { HousingLocation } from '../housinglocation';
-import { HousingService } from '../housing.service';
 
 @Component({
 	selector: 'app-home',
 	standalone: true,
 	imports: [CommonModule, HousingLocationComponent],
 	template: `
-		<section>
-			<form>
-				<input type="text" placeholder="Filter by city" #filter />
-				<button class="primary" type="button" (click)="filterResults(filter.value)">
-					Search
-				</button>
-			</form>
-		</section>
-		<section class="results">
-			<app-housing-location
-				*ngFor="let housingLocation of filteredLocationList"
-				[housingLocation]="housingLocation"
-			></app-housing-location>
-		</section>
+		<div class="block" (click)="func()">Hello world</div>
+		<div>{{ lang ? 'RU' : 'EN' }}</div>
 	`,
 	styleUrl: './home.component.css',
 })
 export class HomeComponent {
-	housingLocationList: HousingLocation[] = [];
-	housingService: HousingService = inject(HousingService);
-	filteredLocationList: HousingLocation[] = [];
+	constructor() {}
 
-	constructor() {
-		this.housingService
-			.getAllHousingLocations()
-			.then((housingLocationList: HousingLocation[]) => {
-				this.housingLocationList = housingLocationList;
-				this.filteredLocationList = housingLocationList;
-			});
-	}
+	lang = true;
 
-	filterResults(text: string) {
-		if (!text) {
-			this.filteredLocationList = this.housingLocationList;
-			return;
-		}
-
-		this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
-			housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
-		);
+	func() {
+		this.lang = !this.lang;
 	}
 }
