d21c72076838b2504ce6849403f5cb7b3517bea6
AndreyDov <dovban.andreyy@rambler.ru>
2024-06-12 20:03:22 +0300

    add: добавлен транслоко

---
+++ transloco.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line no-undef
+module.exports = {
+    rootTranslationsPath: "src/assets/i18n/",
+    langs: ["en", "ru"],
+    keysManager: {},
+};
